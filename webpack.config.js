4000843cf627cd12a0025ddc40c31195c0abddab
maphnew <zcm3118@gmail.com>
2021-10-16 22:40:57 +0900

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,25 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+          publicPath: './dist/',
+          limit: 10000
+        }
+      },
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 }
